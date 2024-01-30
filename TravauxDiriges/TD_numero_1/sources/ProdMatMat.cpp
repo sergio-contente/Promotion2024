bb35b355fe62bec96deec8a4522a609cedc012e3
sergio-contente <sergio.mcontente@gmail.com>
2024-01-30 05:20:44 +0100

    🚑  Using Pramga OMP in CPP

--- TravauxDiriges/TD_numero_1/sources/ProdMatMat.cpp
+++ TravauxDiriges/TD_numero_1/sources/ProdMatMat.cpp
@@ -10,6 +10,10 @@
 namespace {
 void prodSubBlocks(int iRowBlkA, int iColBlkB, int iColBlkA, int szBlock,
                    const Matrix& A, const Matrix& B, Matrix& C) {
+  #ifdef _OPENMP
+    omp_set_num_threads(8); // Remplacez 8 par le nombre souhaité de threads
+  #endif
+  #pragma omp parallel for collapse(3)
   for (int j = iColBlkB; j < std::min(B.nbCols, iColBlkB + szBlock); j++)
   for (int k = iColBlkA; k < std::min(A.nbCols, iColBlkA + szBlock); k++)
   for (int i = iRowBlkA; i < std::min(A.nbRows, iRowBlkA + szBlock); ++i)
@@ -20,6 +24,6 @@ const int szBlock = 32;
 
 Matrix operator*(const Matrix& A, const Matrix& B) {
   Matrix C(A.nbRows, B.nbCols, 0.0);
-  prodSubBlocks(0, 0, 0, szBlock, A, B, C);
+  prodSubBlocks(0, 0, 0, std::max({A.nbRows, B.nbCols, A.nbCols}), A, B, C);
   return C;
 }
