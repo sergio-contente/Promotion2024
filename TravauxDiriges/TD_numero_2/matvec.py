c497d8248f114e47eebb2d22a91c561ddc3a2b44
sergio-contente <sergio.mcontente@gmail.com>
2024-02-04 18:35:41 +0100

    🚀 Finish 1.4 and add time stamps to compare

--- TravauxDiriges/TD_numero_2/matvec.py
+++ TravauxDiriges/TD_numero_2/matvec.py
@@ -1,5 +1,7 @@
 # Produit matrice-vecteur v = A.u
 import numpy as np
+import time
+
 
 # Dimension du problème (peut-être changé)
 dim = 120
@@ -12,5 +14,8 @@ u = np.array([i+1. for i in range(dim)])
 print(f"u = {u}")
 
 # Produit matrice-vecteur
+begin = time.time()
 v = A.dot(u)
+end = time.time()
 print(f"v = {v}")
+print(f"Temps pour calculer le produit (sans parallèlisation): {end - begin} secondes")
