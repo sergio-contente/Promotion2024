57a8bf8c1233a149861230ed584969db587ff958
sergio-contente <sergio.mcontente@gmail.com>
2024-02-04 18:31:02 +0100

    🚧 Implement the produt for rows blocking

---
+++ TravauxDiriges/TD_numero_2/matvec_rows.py
@@ -0,0 +1,36 @@
+from mpi4py import MPI
+import numpy as np
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+dim = 120
+if dim % size != 0:
+    raise ValueError("Matrix size not divisible by number of processes")
+
+Nloc = dim // size
+
+# Initialisation de la matrice A e du vecteur u
+A = np.array([[(i+j) % dim + 1. for i in range(dim)] for j in range(dim)])
+u = np.array([i+1. for i in range(dim)])
+print(f"A={A}")
+print(f"u={u}")
+
+# Chaque processus compute le résultat final du vecteur v
+# Diviser la matrice A em blocs de taille Nloc pour chaque processus
+local_A = A[rank*Nloc:(rank+1)*Nloc, :]
+
+local_v = np.dot(local_A, u.T) 
+
+# Initialisation du vecteur résultat v
+if rank == 0:
+    v = np.empty(dim, dtype=np.float64)
+else:
+    v = None
+
+# Gather les résultats partiels sur le vecteur final
+comm.Gather(local_v, v, root=0)
+
+if rank == 0:
+    print(f"v = {v}")
