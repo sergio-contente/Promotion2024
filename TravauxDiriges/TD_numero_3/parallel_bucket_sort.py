1cd71bccc444219b71acaa7ae8b32077f14df1a6
sergio-contente <sergio.mcontente@gmail.com>
2024-02-27 10:15:50 +0100

    🚧  Working in the bucket sort

--- TravauxDiriges/TD_numero_3/parallel_bucket_sort.py
+++ TravauxDiriges/TD_numero_3/parallel_bucket_sort.py
@@ -1,41 +1,32 @@
 import numpy as np
-from PIL import Image
 import random
 from time import time
 from mpi4py import MPI
 
-#n_buckets = 10
-elements = np.array([0.13, 0.25, 0.15, 0.43, 0.23, 0.49, 0.9, 0.85, 0.75, 0.71, 0.77, 0.65])
-
+elements = np.random.rand(480)
 comm = MPI.COMM_WORLD
-npb = comm.Get_size()
+nbp = comm.Get_size()
 rank = comm.Get_rank()
 
 # Size of the bucket
-NLoc = len(elements) // npb
-chunck = npb + 1
+NLoc = len(elements) // nbp
 
 # Each process represents a bucket
 local_elements = np.zeros(NLoc)
 # Divide the elements into process
-comm.Scatter(elements, local_elements, root=0)
+comm.Scatterv(elements, local_elements, root=0)
 #Sort the local data
 local_elements.sort()
 
-# Take nbp + 1 values at regular intervals
+# Take nbp + 1 values at regular intervals from the buckets
+sampled_elements = local_elements[::max(1,NLoc // (nbp + 1))]
 
-
-
-
-# Gather the sorted local elements back to the root process
-sorted_elements = None
+# Gather values in the bucket array
+bucket_array = None
 if rank == 0:
-    sorted_elements = np.zeros(NLoc * npb)
+    bucket_array = np.empty(NLoc * nbp, dtype=np.float64)
 
-comm.Gather(local_elements, sorted_elements, root=0)
+comm.Gather(sampled_elements, bucket_array, root=0)
 
-#print only one time
-if rank==0:
-    print(sorted_elements)
 
 
