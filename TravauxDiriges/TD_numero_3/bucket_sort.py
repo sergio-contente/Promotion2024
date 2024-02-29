904b72b659eadb55f80d53d35e0e183eed128e33
sergio-contente <sergio.mcontente@gmail.com>
2024-02-29 17:18:14 +0100

    ♻️ Refactor old serial bucket sort algo

--- TravauxDiriges/TD_numero_3/bucket_sort.py
+++ TravauxDiriges/TD_numero_3/bucket_sort.py
@@ -1,24 +1,28 @@
-n_buckets = 10
-elements = [0.13, 0.25, 0.15, 0.43, 0.23, 0.49, 0.9, 0.85, 0.75, 0.71]
-results = []
+import numpy as np
+from time import time
 
-elements_index = [int((i*n_buckets)) for i in elements]
-print(elements_index)
-dictionary = {}
-for index, element in zip(elements_index, elements):
-    if index not in dictionary:
-        dictionary[index] = [element]
-    else:
-        dictionary[index].append(element)
-print(dictionary)
+def bucket_sort(elements, n_buckets=10):
+    min_val, max_val = np.min(elements), np.max(elements)
+    buckets = [[] for _ in range(n_buckets)]
+    
+    interval = (max_val - min_val + 1) / n_buckets
+    
+    for element in elements:
+        index = int((element - min_val) // interval)
+        if index == n_buckets: 
+            index -= 1
+        buckets[index].append(element)
+    
+    sorted_elements = np.concatenate([np.sort(bucket) for bucket in buckets])
+    return sorted_elements
+
+N = 256_000
+elements = np.random.randint(-1000, 1000, size=N, dtype=np.int64)
 
 
-# Compiling sorted lists into the results list
-results = [dictionary[key] for key in dictionary]
+start_time = time()
+sorted_elements = bucket_sort(elements, n_buckets=100)
+end_time = time()
 
-#Flatten the array
-flat_list = []
-for row in results:
-    flat_list.extend(row)
-
-print(flat_list)
+print("First 10 elements ordered:", sorted_elements[:10])
+print("Execution time:", end_time - start_time, "seconds")
