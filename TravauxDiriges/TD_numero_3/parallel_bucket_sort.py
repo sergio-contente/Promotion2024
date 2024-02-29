f81a51f906ff7d480cb851b5b0e8df70af3ad0d9
sergio-contente <sergio.mcontente@gmail.com>
2024-02-29 16:59:50 +0100

    🐛  Resolve not contiguous array problem (FINALLY!)

--- TravauxDiriges/TD_numero_3/parallel_bucket_sort.py
+++ TravauxDiriges/TD_numero_3/parallel_bucket_sort.py
@@ -11,7 +11,15 @@ rank = comm.Get_rank()
 
 # Size of the bucket
 NLoc = N // nbp
-elements = np.random.randint(-32768, 32768, size=NLoc,dtype=np.int64)
+
+if rank == 0:
+    elements = np.random.randint(-1000, 1000, size=N, dtype=np.int64)
+else:
+    elements = None
+
+# Make sure all elements are contiguous before scatter
+if rank == 0:
+    elements = np.ascontiguousarray(elements)
 
 # Each process represents a bucket
 local_elements = np.zeros(NLoc)
@@ -25,6 +33,9 @@ step_pivot = NLoc // nbp
 
 # Take values at regular intervals from the buckets
 pivots = local_elements[step_pivot::step_pivot]
+
+# Make sure all pivots are contiguous before gather
+pivots = np.ascontiguousarray(local_elements[step_pivot::step_pivot][:nbp-1])
 
 #Number of pivots must be 1 less than the number of cores
 pivots = pivots[:nbp-1]
@@ -53,17 +64,22 @@ local_buckets.append( np.array(local_elements[local_elements > glob_pivots[-1]])
 # COLLECT ALL THE LOCAL BUCKETS INTO A GLOBAL BUCKET
 local_values_buckets = None
 
+# Calculates global pivots and distributes into the buckets
+glob_pivots = all_pivots[nbp//2::nbp]
+local_buckets = [np.array(local_elements[local_elements <= glob_pivots[0]])]
+for i in range(1, len(glob_pivots)):
+    local_buckets.append(np.array(local_elements[np.logical_and(local_elements > glob_pivots[i-1], local_elements <= glob_pivots[i])]))
+local_buckets.append(np.array(local_elements[local_elements > glob_pivots[-1]]))
+
+# Colects and sorts the final array
+local_values_buckets = None
 for process in range(nbp):
-    if process == rank :
-        local_values_buckets = comm.gather(local_buckets[process], root=process)
-    else:
-        comm.gather(local_buckets[process], root=process)
+    local_bucket_contiguous = np.ascontiguousarray(local_buckets[process])
+    gathered = comm.gather(local_bucket_contiguous, root=process)
+    if process == rank:
+        local_values_buckets = gathered
+if rank == 0:
+    sorted_loc_values = np.concatenate(local_values_buckets)
+    sorted_loc_values.sort()
+    print("Ordered vector:", sorted_loc_values)
 
-# Sort local values
-my_values = None
-sorted_loc_values = np.concatenate(my_values).astype(np.int64)
-sorted_loc_values.sort()
-
-results_array = comm.gather(sorted_loc_values, root=0)
-if rank==0:
-    print(f"Results:{results_array}")
